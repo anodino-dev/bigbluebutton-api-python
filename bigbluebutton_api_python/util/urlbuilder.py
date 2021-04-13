83e36d842fcc8dd3cbf63c314e363c7c37632562
Ignacio J. Ortega <nacho.ortega@gmail.com>
2021-04-13 17:54:43 +0200

    [BUG] Problems with 2/3 compatibility

--- bigbluebutton_api_python/util/urlbuilder.py
+++ bigbluebutton_api_python/util/urlbuilder.py
@@ -1,6 +1,10 @@
 
 from hashlib import sha1
-from urllib import request
+try:
+    from urllib import quote  # Python 2.X
+except ImportError:
+    from urllib.request import quote  # Python 3+
+    
 from re import match
 
 class UrlBuilder:
@@ -23,7 +27,7 @@ class UrlBuilder:
                 value = "true" if value else "false"
             else:
                 value = str(value)
-            url += key + "=" + request.quote(value.encode('utf-8')) + "&"
+            url += key + "=" + quote(value.encode('utf-8')) + "&"
 
         url += "checksum=" + self.__get_checksum(api_call, params)
         return url
@@ -35,9 +39,9 @@ class UrlBuilder:
                 value = "true" if value else "false"
             else:
                 value = str(value)
-            secret_str += key + "=" + request.quote(value.encode('utf-8')) + "&"
+            secret_str += key + "=" + quote(value.encode('utf-8')) + "&"
         if secret_str.endswith("&"):
             secret_str = secret_str[:-1]
         secret_str += self.securitySalt
-        request.quote(secret_str, 'utf-8')
+#         quote(secret_str, 'utf-8')
         return sha1(secret_str.encode('utf-8')).hexdigest()
